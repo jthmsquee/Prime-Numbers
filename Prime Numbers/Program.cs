0ec86211f0784ff2c821ff7548803aeb4a304107
jthmsquee <christian.ascencio@gmail.com>
2016-09-27 23:06:50 -0700

    done

--- Prime Numbers/Program.cs
+++ Prime Numbers/Program.cs
@@ -10,6 +10,40 @@ namespace Prime_Numbers
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Type in a Number");
+
+            try
+            {
+                int numberEntered = Convert.ToInt32(Console.ReadLine());
+                List<int> primeNumbers = new List<int>();
+                for (int i = numberEntered - 1; i > 1; i--)
+                {
+                    if (isItPrime(i) == true)
+                    {
+                        primeNumbers.Add(i);
+                    }
+                }
+                string myList = String.Join(",", primeNumbers);
+                Console.WriteLine(myList);
+            }
+            catch
+            {
+                Console.WriteLine("Exception Caught");
+            }
+            Console.ReadLine();
+        }
+        private static bool isItPrime(int aNumber)
+        {
+            int largestDivisableNumber = aNumber / 2;
+            for (int i = 2; i <= largestDivisableNumber; i++)
+            {
+                if (aNumber % i == 0)
+                {
+                    return false;
+                }
+            }
+            return true;
+
         }
     }
 }
